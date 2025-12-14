fcd7d05e326cc30a63e808d261dd3f99eae606dd
ufologhome <38780109+ufologhome@users.noreply.github.com>
2025-12-14 18:54:19 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.helloandroid
 
-import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val text = TextView(this)
-        text.text = "Привет! APK собран через GitHub 🚀"
-        text.textSize = 20f
-
-        setContentView(text)
+        val button = findViewById<Button>(R.id.btnOpen)
+        button.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.youtube.com")
+            )
+            startActivity(intent)
+        }
     }
 }
